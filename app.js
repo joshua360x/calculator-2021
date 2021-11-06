2f46b431a6857d8cafec12fae9c957bcb1f0e7d1
joshua360x <jkw5499@psu.edu>
2021-11-05 20:01:04 -0400

    Made 4 calculator functions

--- app.js
+++ app.js
@@ -1,8 +1,131 @@
 // import functions and grab DOM elements
+import { add, subtract, multiply, divide } from './math-utils.js';
+
 
 // initialize global state
+const addIncrement = document.getElementById('addMany');
+const subtractIncrement = document.getElementById('subMany');
+const multiplyIncrement = document.getElementById('multiMany');
+const divisionIncrement = document.getElementById('divMany');
+
+
+
+
+
+const addButton = document.getElementById('addButton');
+const subButton = document.getElementById('subButton');
+const multiButton = document.getElementById('multiButton');
+const divButton = document.getElementById('divButton');
+
+
+const addInput1 = document.getElementById('inputADD1'); 
+const addInput2 = document.getElementById('inputADD2'); 
+
+const subInput1 = document.getElementById('inputSUB1'); 
+const subInput2 = document.getElementById('inputSUB2'); 
+
+const multiInput1 = document.getElementById('inputMULTI1'); 
+const multiInput2 = document.getElementById('inputMULTI2'); 
+
+const divInput1 = document.getElementById('inputDIV1'); 
+const divInput2 = document.getElementById('inputDIV2'); 
+
+
+const answerAdd = document.getElementById('addAnswer'); 
+const answerSub = document.getElementById('answerSub'); 
+const answerMulti = document.getElementById('answerMulti'); 
+const answerDiv = document.getElementById('answerDiv'); 
+
 
 // set event listeners 
   // get user input
   // use user input to update state 
-  // update DOM to reflect the new state
+   // update DOM to reflect the new state
+
+
+
+let addCounter = 0;
+let subtractCounter = 0;
+let multiplyCounter = 0;
+let divideCounter = 0;
+
+addButton.addEventListener('click', () => {
+
+  const number1 = +addInput1.value;
+  const number2 = +addInput2.value;
+
+  const result = add(number1, number2);
+
+answerAdd.textContent = result;
+
+addCounter++;
+
+addIncrement.textContent = addCounter;
+
+
+
+
+})
+
+
+subButton.addEventListener('click', () => {
+
+  const number1 = +subInput1.value;
+  const number2 = +subInput2.value;
+
+  const result = subtract(number1, number2);
+
+answerSub.textContent = result;
+
+
+// counter 
+
+subtractCounter++;
+
+subtractIncrement.textContent = subtractCounter;
+
+
+})
+
+
+multiButton.addEventListener('click', () => {
+
+  const number1 = +multiInput1.value;
+  const number2 = +multiInput2.value;
+
+  const result = multiply(number1, number2);
+
+answerMulti.textContent = result;
+
+
+multiplyCounter++;
+
+multiplyIncrement.textContent = multiplyCounter;
+
+
+})
+
+
+divButton.addEventListener('click', () => {
+
+  const number1 = +divInput1.value;
+  const number2 = +divInput2.value;
+
+  const result = divide(number1, number2);
+
+answerDiv.textContent = result;
+
+divideCounter++;
+
+divisionIncrement.textContent = divideCounter;
+
+})
+
+
+
+
+
+
+
+
+
--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,61 @@
 // IMPORT MODULES under test here:
-// import { example } from '../example.js';
+import { add, subtract, multiply, divide } from '../math-utils';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('add() should take 1 and 1 and return 2', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
-    
+    const expected = 2;
+
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = add(1, 1);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+
+test('subtract() should take 1 and 1 and return 0', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 0;
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = subtract(1, 1);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+
+test('multiply() should take 1 and 1 and return 1', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 1;
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = multiply(1, 1);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+
+test('divide() should take 2 and 1 and return 1', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 2;
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = divide(2, 1);
 
     //Expect
     // Make assertions about what is expected versus the actual result
